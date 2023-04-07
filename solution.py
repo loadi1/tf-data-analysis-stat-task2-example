1999e1999253aabf973b07c2fc46f7a410bfb571
loadi1 <70744346+loadi1@users.noreply.github.com>
2023-04-07 19:11:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,6 +13,6 @@ def solution(p: float, x: np.array) -> tuple:
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     left = len(x)*loc/norm.ppf(p)/np.sqrt(50)
     right = len(x)*loc/norm.ppf(alpha)/np.sqrt(50)
-    #return loc - scale * norm.ppf(1 - alpha / 2), \
-    #       loc - scale * norm.ppf(alpha / 2)
-    return left, right
+    return (loc - scale * norm.ppf(1 - alpha / 2))/np.sqrt(50), \
+           loc - scale * norm.ppf(alpha / 2)/np.sqrt(50)
+
